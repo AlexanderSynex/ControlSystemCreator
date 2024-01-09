8448358b60307bceac6b292c918b0cb7539eb771
AlexanderSynex <malikoff.at@gmail.com>
2024-01-09 20:30:19 +0300

    Added test files

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from PyQt6.QtWidgets import QApplication, QWidget
 
+from packages import QSystemEditor
+
 def main():
     app = QApplication([])
     
-    window = QWidget()
+    window = QSystemEditor()
     window.show()
     
     return app.exec()
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 #!/usr/bin/env python3.10
+
 clear && python main.py
