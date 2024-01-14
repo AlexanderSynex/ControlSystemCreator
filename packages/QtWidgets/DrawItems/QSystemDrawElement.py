6dd273d797f37570aeeab5e3e0166de5dc205790
AlexanderSynex <malikoff.at@gmail.com>
2024-01-14 15:23:52 +0300

    Added parent to System Draw ELement

--- packages/QtWidgets/DrawItems/QSystemDrawElement.py
+++ packages/QtWidgets/DrawItems/QSystemDrawElement.py
@@ -3,5 +3,10 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 
 class QSystemDrawElement(QGraphicsRectItem):
-    def __init__(self):
-        super().__init__(0, 0, 50, 50)
+    def __init__(self, name='', parent=None):
+        super().__init__(0, 0, 50, 50, parent=parent)
+        
+        self.__input_ports = {} # Coordinates
+        self.__output_ports = {} # Coordinates
+
+        self.nameItem = QGraphicsSimpleTextItem(name, self)
