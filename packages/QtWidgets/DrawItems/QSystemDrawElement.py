d0bd97bceeb528fe0a1329a8cf9dbec0fab3b73e
AlexanderSynex <malikoff.at@gmail.com>
2024-01-14 16:08:27 +0300

    Added displaying system name near system rectangle

--- packages/QtWidgets/DrawItems/QSystemDrawElement.py
+++ packages/QtWidgets/DrawItems/QSystemDrawElement.py
@@ -4,9 +4,19 @@ from PyQt6.QtGui import *
 
 class QSystemDrawElement(QGraphicsRectItem):
     def __init__(self, name='', parent=None):
-        super().__init__(0, 0, 50, 50, parent=parent)
+        super().__init__(0, 0, 100, 50, parent=parent)
         
-        self.__input_ports = {} # Coordinates
-        self.__output_ports = {} # Coordinates
+        self.__name = name
+        
+        self.__input_ports = {}
+        self.__output_ports = {}
 
-        self.nameItem = QGraphicsSimpleTextItem(name, self)
+        self.__nameItem = QGraphicsTextItem(self.__name, self)
+        
+        self.__update_text()
+    
+    def __update_text(self):
+        rect = self.__nameItem.boundingRect()
+        rect.moveBottomLeft(self.boundingRect().topLeft())
+        
+        self.__nameItem.setPos(rect.topLeft())
