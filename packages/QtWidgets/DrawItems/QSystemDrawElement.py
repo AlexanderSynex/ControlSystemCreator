b6263f778f5af7b6d6ef9275844b0f1d079c18ea
AlexanderSynex <malikoff.at@gmail.com>
2024-01-14 16:21:16 +0300

    Added bouinding box property (collision) for System Draw Item

--- packages/QtWidgets/DrawItems/QSystemDrawElement.py
+++ packages/QtWidgets/DrawItems/QSystemDrawElement.py
@@ -3,8 +3,9 @@ from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 
 class QSystemDrawElement(QGraphicsRectItem):
+    __width, __height = 100, 50
     def __init__(self, name='', parent=None):
-        super().__init__(0, 0, 100, 50, parent=parent)
+        super().__init__(0, 0, __width, __height, parent=parent)
         
         self.__name = name
         
@@ -15,8 +16,14 @@ class QSystemDrawElement(QGraphicsRectItem):
         
         self.__update_text()
     
+    
     def __update_text(self):
         rect = self.__nameItem.boundingRect()
         rect.moveBottomLeft(self.boundingRect().topLeft())
         
         self.__nameItem.setPos(rect.topLeft())
+        
+    @property
+    def collision(self):
+        text_rect = self.__nameItem.boundingRect()
+        return __width + text_rect.width(), __height + text_rect.height()
