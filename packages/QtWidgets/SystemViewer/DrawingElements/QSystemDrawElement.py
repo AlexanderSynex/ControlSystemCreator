a942cbfbff264b23ea51eb5e58cd66709c27e5a8
AlexanderSynex <malikoff.at@gmail.com>
2024-01-26 21:50:29 +0300

    Refactored drawing system

--- packages/QtWidgets/SystemViewer/DrawingElements/QSystemDrawElement.py
+++ packages/QtWidgets/SystemViewer/DrawingElements/QSystemDrawElement.py
@@ -10,30 +10,20 @@ class QSystemDrawElement(QGraphicsItem):
     def __init__(self, name='', parent=None):
         super().__init__(parent=parent)
         
-        self.__name = QStaticText()
-        self.__name.setText(name)
+        self.__name = QStaticText(name)
+        self.__ins = {}     #In Points
+        self.__outs = {}    #Out Points
         
         if not SystemManager().exists(name):
             return
         
         sys = SystemManager().get_instance(name)
         
-        self.__ins = {}     #In Points
-        self.__outs = {}    #Out Points
-        
         for key in sys.input_keys:
             self.__ins[key] = QPointF(0, 0)
             
         for key in sys.output_keys:
             self.__outs[key] = QPointF(0, 0)
-            
-    
-    def __update_text(self):
-        rect = self.__nameItem.boundingRect()
-        rect.moveBottomLeft(self.boundingRect().topLeft())
-        
-        self.__nameItem.setPos(rect.topLeft())
-        
     
 
     def boundingRect(self):
@@ -51,24 +41,30 @@ class QSystemDrawElement(QGraphicsItem):
         painter.drawStaticText(text_point, QStaticText(self.__name))
         
         # Calculating ports' positions
-        y_step = self.__height / (len(self.__ins) + 1)
-        for i, port in enumerate(self.__ins):
-            self.__ins[port].setX(self.boundingRect().x())
-            self.__ins[port].setY(self.boundingRect().top() + y_step * (i + 1))
-            
-        
-        y_step = self.__height / (len(self.__outs) + 1)
-        for i, port in enumerate(self.__outs):
-            self.__outs[port].setX(self.boundingRect().right())
-            self.__outs[port].setY(self.boundingRect().top() + y_step * (i + 1))
-        
+        self.__update_ports()
         
         # Drawing connection points
-        r = 3
+        r = 2
         painter.setBrush(QBrush(Qt.GlobalColor.green))
-        for port in self.__ins:
-            painter.drawEllipse(self.__ins[port], r, r)
+        for (port, point) in self.__ins.items():
+            print(f"Port = {port}, Point = {point}")
+            painter.drawEllipse(point, r, r)
 
         painter.setBrush(QBrush(Qt.GlobalColor.red))
-        for port in self.__outs:
-            painter.drawEllipse(self.__outs[port], r, r)
+        for (port, point) in self.__outs.items():
+            print(f"Port = {port}, Point = {point}")
+            painter.drawEllipse(point, r, r)
+            
+    
+    def __update_ports(self):
+        y_step = self.__height / (len(self.__ins) + 1)
+        for i, (port_name, point) in enumerate(self.__ins.items()):
+            point.setX(self.boundingRect().x())
+            point.setY(self.boundingRect().top() + y_step * (i + 1))
+            self.__ins[port_name] = point
+        
+        y_step = self.__height / (len(self.__outs) + 1)
+        for i, (port_name, point) in enumerate(self.__outs.items()):
+            point.setX(self.boundingRect().right())
+            point.setY(self.boundingRect().top() + y_step * (i + 1))
+            self.__outs[port_name] = point
