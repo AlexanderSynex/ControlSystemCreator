683f275c0878cf423489ab31d8c66dca2e0ff3b2
AlexanderSynex <malikoff.at@gmail.com>
2024-02-02 14:34:53 +0300

    Modified loading systems to updated json format

--- packages/SystemModule/System/SystemDataWrapper.py
+++ packages/SystemModule/System/SystemDataWrapper.py
@@ -19,45 +19,18 @@ class SystemDataWrapper(metaclass=Singleton):
         outputs = [name for name in sys.output_keys]
         
         return dict(name=system_name, inputs=inputs, outputs=outputs)
-    
-    
-    def from_dict(cls, system_dict : dict):
-        if not all(key in system_dict for key in ('name',
-                                                  'inputs', 
-                                                  'outputs')):
-            return
-        
-        input_keys = ConnectionDataWrapper().from_dicts(system_dict['inputs'])
-        output_keys = ConnectionDataWrapper().from_dicts(system_dict['outputs'])
-        
-        SystemManager().get_instance(name=system_dict['name'],
-                                     Inputs=input_keys,
-                                     Outputs=output_keys)
-        
-        ConnectionManager().rebase_internal_connections()
-    
+
     
     def to_json(cls, system_name : str) -> str:
         if not SystemManager().exists(system_name):
             return None
         
         return json.dumps(cls.to_dict(system_name=system_name), indent=2)
-    
-    
-    def from_json(cls, path : str):
-        with open(path) as file:
-            raw_data = json.load(file)
-            
-            if 'systems' not in raw_data:
-                return
-            
-            for system in raw_data['systems']:
-                cls.from_dict(system)
-    
+
     
     def all_to_dict(cls):
         if SystemManager().empty():
-            return dict(systems=[])
+            return dict(signals=[],systems=[])
         
         signals=[]
         for name in ConnectionManager().get_keys():
@@ -71,10 +44,34 @@ class SystemDataWrapper(metaclass=Singleton):
     
     
     def all_to_json(cls) -> str:
-        if SystemManager().empty():
-            return json.dumps(systems=[])
-        
         return json.dumps(cls.all_to_dict(), indent=2)
     
     
+    def from_dict(cls, system_dict : dict):
+        if not all(key in system_dict for key in ('name',
+                                                  'inputs', 
+                                                  'outputs')):
+            return
+        
+        input_keys = system_dict['inputs']
+        output_keys = system_dict['outputs']
+        
+        SystemManager().get_instance(name=system_dict['name'],
+                                     Inputs=input_keys,
+                                     Outputs=output_keys)
+        
+        ConnectionManager().rebase_internal_connections()
     
+    
+    def from_json(cls, path : str):
+        with open(path) as file:
+            raw_data = json.load(file)
+            
+            if not all(key in raw_data for key in ('systems', 'signals')):
+                return
+            
+            for signal in raw_data['signals']:
+                ConnectionDataWrapper().from_dict(signal)
+            
+            for system in raw_data['systems']:
+                cls.from_dict(system)
