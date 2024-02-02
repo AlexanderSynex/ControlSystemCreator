b87a8b758ac92e95b1ce49079094d894c95b8386
AlexanderSynex <malikoff.at@gmail.com>
2024-02-02 11:53:57 +0300

    Modified signal draw element members to protected access

--- packages/QtWidgets/SystemViewer/DrawingElements/QSignalDrawElement.py
+++ packages/QtWidgets/SystemViewer/DrawingElements/QSignalDrawElement.py
@@ -13,32 +13,32 @@ class QSignalDrawElement(QGraphicsItem):
     def __init__(self, name = "", parent=None):
         super().__init__(parent=parent)
         
-        self.__name = name
-        self.__value = 0
+        self._name = name
+        self._value = 0
         
         self._from_point = QPointF()
         self._to_points = []
 
-        self.__from_point, self.__to_points = self.__parse_link_to_points()
+        self._from_point, self._to_point = self._parse_link_to_points()
 
         print(f"Signal={self.name} From=({self.from_point.x()}, {self.from_point.y()}), To={self.to_points}")
     
     
     def update_value(self):
-        if (ConnectionManager().exists(self.__name)):
-            self.__value = ConnectionManager().get_instance(self.__name).value
+        if (ConnectionManager().exists(self._name)):
+            self._value = ConnectionManager().get_instance(self._name).value
 
     @property
     def from_point(self):
-        return self.__from_point
+        return self._from_point
     
     @property
     def to_points(self):
-        return self.__to_points
+        return self._to_point
 
     @property
     def name(self):
-        return self.__name
+        return self._name
     
 
     def boundingRect(self):
@@ -55,23 +55,23 @@ class QSignalDrawElement(QGraphicsItem):
     
 
     def paint(self, painter, option, widget):
-        self.__from_point, self.__to_points = self.__parse_link_to_points()
+        self._from_point, self._to_point = self._parse_link_to_points()
         for to_point in self.to_points:
             painter.drawLine(self.from_point, to_point)
         
         self.update_value()
-        painter.drawText(self.from_point, f"{self.__name} (value={self.__value})")
+        painter.drawText(self.from_point, f"{self._name} (value={self._value})")
 
         
     # Собирает точки для конкретной связи
-    def __parse_link_to_points(self):
+    def _parse_link_to_points(self):
         p1, p2s = None, []
         margin = 50
         
-        if not ConnectionManager().exists(self.__name):
+        if not ConnectionManager().exists(self._name):
             return p1, p2s
         
-        link = ConnectionManager().get_instance(self.__name)
+        link = ConnectionManager().get_instance(self._name)
         if DrawnItemsManager().system_exists(link.from_system):
             system_item = DrawnItemsManager().get_system(link.from_system)
 
